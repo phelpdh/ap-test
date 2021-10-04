0830bc169e2fcf0f2cae15f0eac98e78f101b115
dan_phelps <dan_phelps@dell.com>
2021-10-04 09:29:47 -0500

    mod again

--- src/main/java/com/dan/Sample.java
+++ src/main/java/com/dan/Sample.java
@@ -2,6 +2,6 @@ package com.dan;
 
 public class Sample {
     public static void main(String[] args) {
-        System.out.println("Sample App Changed Again...");
+        System.out.println("Sample App Changed Publish");
     }
 }
